f222f89bc0dc63e28adb2e78d1a5df9ab4870bcd
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-04-29 19:56:02 -0300

    chore: clean up code structure and remove unused code blocks

--- src/main.py
+++ src/main.py
@@ -1,14 +1,33 @@
-from src.models.text_node import TextNode, TextType
+import os
+import shutil
 
 
-print("Hello World!")
+def copy_recursive(src, dest):
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dest_path = os.path.join(dest, item)
+
+        if os.path.isfile(src_path):
+            print(f"Copying {src_path} to {dest_path}")
+            shutil.copy(src_path, dest_path)
+        else:
+            copy_recursive(src_path, dest_path)
+
 
 def main():
-    print("This is the main function.")
-    node = TextNode("This is some anchor text",TextType.LINK, "https://www.boot.dev")
-    
-    print(node)
-    
+    root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    static_path = os.path.join(root_dir, "static")
+    public_path = os.path.join(root_dir, "public")
+
+    print(f"Cleaning public directory at {public_path}...")
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+
+    print(f"Copying static files from {static_path} to {public_path}...")
+    copy_recursive(static_path, public_path)
 
 if __name__ == "__main__":
     main()
